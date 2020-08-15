ba04fb99866f6c96f7c54f198cca60dcbe9f97bd
divyavala3 <divyavala3@gmail.com>
2020-08-15 20:09:19 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import {FlatList, Image, Dimensions,View} from 'react-native';
+import * as Animatable from 'react-native-animatable';
+
+import {Header} from "react-native-elements";
+let users = require('./userList.json');
+let {width: screenWidth} = Dimensions.get('window');
+
+class AnimatedImages extends Component {
+  render() {
+    return (
+      <Animatable.View
+        style={{flex: 1, alignItems: 'center'}}
+        animation="fadeInUpBig"
+        delay={this.props.imageIndex * 100}
+        useNativeDriver={true}>
+        {this.props.children}
+      </Animatable.View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default class App extends Component {
+  renderImages = item => {
+    return (
+      <AnimatedImages imageIndex={item.index}>
+        <Image
+          source={{uri: item.item.image}}
+          style={{
+            height: screenWidth / 4,
+            width: screenWidth / 4,
+          }}
+        />
+      </AnimatedImages>
+    );
+  };
+  render() {
+    return (
+ <View style={{flex:1}}>
+   <Header centerComponent={{text:"Header"}}/>
+          <FlatList
+            horizontal={false}
+            numColumns={4}
+            data={users}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={this.renderImages}
+          />
+          </View>
+     
+    );
+  }
+}
