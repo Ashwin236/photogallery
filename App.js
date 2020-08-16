579908463101b667e8992d2b73051f377339b958
divyavala3 <divyavala3@gmail.com>
2020-08-17 00:16:53 +0530

    made responsive

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
 import React, {Component} from 'react';
 
-import {FlatList, Image, Dimensions,View} from 'react-native';
+import {FlatList, Image, Dimensions,View,Modal,Text, TouchableOpacity,StyleSheet} from 'react-native';
 import * as Animatable from 'react-native-animatable';
 
 import {Header} from "react-native-elements";
-let users = require('./userList.json');
-let {width: screenWidth} = Dimensions.get('window');
+var users = require('./userList.json');
+var {width: screenWidth} = Dimensions.get('window');
 
 class AnimatedImages extends Component {
   render() {
     return (
       <Animatable.View
         style={{flex: 1, alignItems: 'center'}}
-        animation="fadeInUpBig"
+        animation="zoomIn"
         delay={this.props.imageIndex * 100}
         useNativeDriver={true}>
         {this.props.children}
@@ -21,9 +21,22 @@ class AnimatedImages extends Component {
   }
 }
 export default class App extends Component {
+constructor(){
+  super();
+  this.state={
+    modalVisible:false,
+    modalImage:null
+  }
+}
+
+setModalVisible(visible,image){
+this.setState({modalVisible:visible,modalImage:image})
+}
   renderImages = item => {
+console.log(item);
     return (
       <AnimatedImages imageIndex={item.index}>
+        <TouchableOpacity key={item.index}onPress={()=>{this.setModalVisible(true,item.item.image)}}>
         <Image
           source={{uri: item.item.image}}
           style={{
@@ -31,13 +44,15 @@ export default class App extends Component {
             width: screenWidth / 4,
           }}
         />
+        </TouchableOpacity>
       </AnimatedImages>
     );
   };
   render() {
+    
     return (
  <View style={{flex:1}}>
-   <Header centerComponent={{text:"Header"}}/>
+   <Header centerComponent={{text:"Photos"}}/>
           <FlatList
             horizontal={false}
             numColumns={4}
@@ -45,8 +60,35 @@ export default class App extends Component {
             keyExtractor={(item, index) => index.toString()}
             renderItem={this.renderImages}
           />
+
+          <Modal style={styles.modal} animationType={"fade"}transparent={true}visible={this.state.modalVisible}>
+           <View style={styles.modal}>
+            <Text style={styles.text} onPress={()=>{this.setModalVisible(false)}}>close</Text>
+            <View >
+<Image source={{uri: this.state.modalImage}}
+          style={{
+            height: screenWidth ,
+            width: screenWidth ,
+          }}
+        />
+</View>
+            </View>
+          </Modal>
           </View>
      
     );
   }
 }
+const styles=StyleSheet.create({
+  modal:{
+    flex:1,
+    padding:40,
+    paddingLeft:0,
+    backgroundColor:'rgba(0,0,0,0.9)'
+  },
+  text:{
+    color:"#ffff",
+    marginTop:200
+    
+  }
+})
